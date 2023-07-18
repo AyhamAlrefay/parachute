07b320831b2b7ab09ebc83abd3aeb864ccf6c733
Ayham Alrefay <ayhamalrefay@gmail.com>
2023-07-18 23:01:12 +0300

    add any thing

--- src/script.js
+++ src/script.js
@@ -3,14 +3,15 @@ import * as THREE from "three";
 import * as dat from "dat.gui";
 import loadGrassTextures from "./config/GrassTexture";
 import { loadModelsGltf } from "./config/ModelsGltf";
-import { loadModelsObj ,animateFeet} from "./config/ModelsObj";
+import { loadModelsObj } from "./config/ModelsObj";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
-import World from "./physics/world";
 import Parachutist from "./physics/parachutist";
+
 // GUI setup
 const gui = new dat.GUI();
 gui.close();
+
 const updateGUIWidth = (mediaQuery) => {
   gui.width = mediaQuery.matches ? 150 : 250;
 };
@@ -20,76 +21,65 @@ const mediaQuery = window.matchMedia("(max-width: 425px)");
 updateGUIWidth(mediaQuery);
 mediaQuery.addListener(updateGUIWidth);
 
-
-let windAngle=Math.PI / 2;
-let axesHelper= false;
-let radiusUmbrella= 0.5;
-let height= 1000;
-let manMass= 100;
-let umbrellaMass= 10;
-let windSpeed = new THREE.Vector3(0, 0, 0); 
+let windAngle = Math.PI / 2;
+let axesHelper = false;
+let radiusUmbrella = 0.5;
+let height = 10000;
+let manMass = 100;
+let umbrellaMass = 10;
+let windSpeed = new THREE.Vector3(0, 0, 0);
 const paramters = {
   windAngle: Math.PI / 2,
   axesHelper: false,
   radiusUmbrella: 0.5,
-  height: 1000,
+  height: 10000,
   manMass: 100,
-  umbrellaMass:10,
+  umbrellaMass: 10,
 };
 
 
+parachutefolder
+  .add(paramters, "windAngle", 0, 100, 0.1)
+  .name("windAngle")
+  .onChange(() => {
+    windAngle = paramters.windAngle;
+  });
 
 parachutefolder
-.add(paramters, "windAngle", 0, 100, 0.1)
-.name("windAngle")
-.onChange(() => {
-  windAngle = paramters.windAngle;
-});
+  .add(paramters, "axesHelper")
+  .name("axesHelper")
+  .onChange(() => {
+    axesHelper = paramters.axesHelper;
+  });
+parachutefolder
+  .add(paramters, "radiusUmbrella", 0, 5, 0.1)
+  .name("radiusUmbrella")
+  .onChange(() => {
+    radiusUmbrella = paramters.radiusUmbrella;
+  });
+parachutefolder
+  .add(paramters, "height", 0, 70000, 10)
+  .name("height")
+  .onChange(() => {
+    modelsGroup.position.y=paramters.height;
+    height = paramters.height;
+  });
 
 parachutefolder
-.add(paramters, "axesHelper")
-.name("axesHelper")
-.onChange(() => {
-  axesHelper = paramters.axesHelper;
-});
-parachutefolder
-.add(paramters, "radiusUmbrella", 0, 5, 0.1)
-.name("radiusUmbrella")
-.onChange(() => {
-  radiusUmbrella = paramters.radiusUmbrella;
-});
-parachutefolder
-.add(paramters, "height", 0, 20000, 10)
-.name("height")
-.onChange(() => {
-  height = paramters.height;
-});
+  .add(paramters, "manMass", 0, 1000, 1)
+  .name("manMass")
+  .onChange(() => {
+    manMass = paramters.manMass;
+  });
 
 parachutefolder
-.add(paramters, "manMass", 0, 1000, 1)
-.name("manMass")
-.onChange(() => {
-  manMass = paramters.manMass;
-});
+  .add(paramters, "umbrellaMass", 0, 100, 1)
+  .name("umbrellaMass")
+  .onChange(() => {
+    umbrellaMass = paramters.umbrellaMass;
+  });
 
-
-parachutefolder
-.add(paramters, "umbrellaMass", 0, 100, 1)
-.name("umbrellaMass")
-.onChange(() => {
-  umbrellaMass = paramters.umbrellaMass;
-});
-
-
-
-// Add GUI controls for all parameters as text field
-// for (let key in paramters) {
-//   parachutefolder.add(paramters, key).onChange(() => {
-//     parachute[key] = paramters[key];
-//   });
-// }
-
-
+ 
 // Scene setup
 const size = {
   width: window.innerWidth,
@@ -106,17 +96,17 @@ const modelsGroup = new THREE.Group();
 
 // Background setup
 const texture = textureLoader.load("textures/skybox/FS002_Day.png", () => {
-  const rt = new THREE.WebGLCubeRenderTarget(texture.image.height*5);
+  const rt = new THREE.WebGLCubeRenderTarget(texture.image.height * 5);
   rt.fromEquirectangularTexture(renderer, texture);
   scene.background = rt.texture;
 });
 // Models
-let manModel, airplanModel,parachuteModel;
+let manModel, airplanModel, parachuteModel;
 // Load textures and models
 const grassTextures = loadGrassTextures(textureLoader);
-loadModelsGltf(scene, gltfLoader,(parachute)=>{
-  parachuteModel=parachute;
- modelsGroup.add(parachuteModel);
+loadModelsGltf(scene, gltfLoader, (parachute) => {
+  parachuteModel = parachute;
+  modelsGroup.add(parachuteModel);
 });
 loadModelsObj(scene, objLoader, (loadedModel1) => {
   manModel = loadedModel1;
@@ -124,35 +114,27 @@ loadModelsObj(scene, objLoader, (loadedModel1) => {
 }, (loadedModel2) => {
   airplanModel = loadedModel2;
 });
-
-
 scene.add(modelsGroup);
 
-
-
-
 // Event listeners
-
 //document.addEventListener('mousemove', onMouseMove);
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   if (event.key === 'o') {
-    scaleParachute();  
+    scaleParachute();
   }
-  if(event.key==='w'){
+  if (event.key === 'w') {
     physics();
   }
 });
 window.addEventListener("dblclick", toggleFullScreen);
 window.addEventListener("resize", onWindowResize);
 
-
-
 function onMouseMove(event) {
   camera.rotation.y -= event.movementX * 0.004;
   camera.rotation.x -= event.movementY * 0.004;
 }
 
- function toggleFullScreen() {
+function toggleFullScreen() {
   const fullScreen = document.fullscreenElement || document.webkitFullscreenElement;
   if (!fullScreen) {
     if (canvas.requestFullscreen) {
@@ -205,20 +187,32 @@ const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(size.width, size.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-modelsGroup.position.y=height;
+modelsGroup.position.y = height;
 
 const p = new Parachutist();
- const valuesContainer = document.getElementById("values-container");
-let displacement = new THREE.Vector3(); 
-let velocity = new THREE.Vector3(0, 0, 0); 
-let tensileForce = new THREE.Vector3(0, 0, 0); 
-let surfaceArea = Math.PI*radiusUmbrella*radiusUmbrella;
+const valuesContainer = document.getElementById("values-container");
+let displacement = new THREE.Vector3();
+let velocity = new THREE.Vector3(0, 0, 0);
+let tensileForce = new THREE.Vector3(0, 0, 0);
+let surfaceArea = Math.PI * radiusUmbrella * radiusUmbrella;
 const groundPosition = new THREE.Vector3(0, 0, 0);
-
+let newVelocity=new THREE.Vector3(0, 0, 0), newDisplacement=new THREE.Vector3(0, 0, 0), newAcceleration=new THREE.Vector3(0, 0, 0), dragForce=new THREE.Vector3(0, 0, 0), weightForce =new THREE.Vector3(0, 0, 0);
+valuesContainer.innerHTML = `
+<p>Position: ${modelsGroup.position.x.toFixed(2)}, ${modelsGroup.position.y.toFixed(2)}, ${modelsGroup.position.z.toFixed(2)}</p>
+<p>Acceleration: ${newAcceleration.x.toFixed(2)}, ${newAcceleration.y.toFixed(2)}, ${newAcceleration.z.toFixed(2)}</p>
+<p>Velocity: ${velocity.x.toFixed(2)}, ${velocity.y.toFixed(2)}, ${velocity.z.toFixed(2)}</p>
+<p>DragForce: ${dragForce.x.toFixed(2)}, ${dragForce.y.toFixed(2)}, ${dragForce.z.toFixed(2)}</p>
+<p>WeightForce: ${weightForce.x.toFixed(2)},${weightForce.y.toFixed(2)}, ${dragForce.z.toFixed(2)}</p>
+`;
 const update = (delta) => {
   if (modelsGroup.position.y > groundPosition.y) {
-    let { newVelocity, newDisplacement, newAcceleration, dragForce, weightForce } = p.calculateDisplacement(delta, manMass, umbrellaMass, velocity, displacement, surfaceArea, windSpeed, tensileForce);
-    velocity.copy(newVelocity);
+   let result = p.calculateDisplacement(delta, manMass, umbrellaMass, velocity, displacement, surfaceArea, windSpeed, tensileForce);
+   newVelocity=result.newVelocity;
+   newDisplacement=result.newDisplacement;
+   newAcceleration=result.newAcceleration;
+   dragForce=result.dragForce;
+   weightForce=result.weightForce;
+   velocity.copy(newVelocity);
     displacement.copy(newDisplacement);
     modelsGroup.position.copy(p.updatePosition(modelsGroup.position, displacement));
     valuesContainer.innerHTML = `
@@ -230,52 +224,42 @@ const update = (delta) => {
     `;
   } else {
     velocity.set(0, 0, 0);
-  
   }
 };
 
+let scaleOfParrchute = 0;
 
-
-
-
-
-
-let scaleOfParrchute=0;// Animation loop
-
-  
+// Animation loop
 const tick = () => {
   if (airplanModel) {
-    airplanModel.position.x+=3;
-    
+    airplanModel.position.x += 3;
   }
-   
   renderer.render(scene, camera);
   requestAnimationFrame(tick);
 };
 
 const scaleParachute = () => {
   if (scaleOfParrchute <= 50) {
-    parachuteModel.position.y=scaleOfParrchute+30;
+    parachuteModel.position.y = scaleOfParrchute + 30;
     parachuteModel.scale.set(scaleOfParrchute, scaleOfParrchute, scaleOfParrchute);
     scaleOfParrchute += 1;
     requestAnimationFrame(scaleParachute);
   }
 };
+
 const clock = new THREE.Clock();
 let oldElapsedTime = 0;
 const physics = () => {
   console.log(paramters.gravity);
   const elapsedTime = clock.getElapsedTime();
   camera.position.set(0, modelsGroup.position.y + 20, 720);
- 
+
   const deltaTime = elapsedTime - oldElapsedTime;
 
-  update(deltaTime ); 
+  update(deltaTime/10);
   oldElapsedTime = elapsedTime;
 
   requestAnimationFrame(physics);
-}
-
-
+};
 
 tick();
